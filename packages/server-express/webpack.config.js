1677b6d15d883230a9a42e44effc23a2a1ee0103
Carlos Kazuo <ninetails.nine@gmail.com>
2018-11-20 05:55:38 -0200

    refactor(server-express): added public to build & folder assets

--- packages/server-express/webpack.config.js
+++ packages/server-express/webpack.config.js
@@ -8,23 +8,28 @@ require('./loadenv')
 const dist = join(__dirname, 'dist')
 
 const mode = process.env.NODE_ENV || 'development'
-const devtool = process.env.NODE_ENV === 'development' ? 'cheap-eval-source-map' : 'source-map'
-const clientFilename = process.env.NODE_ENV === 'production' ? '[name].[hash].js' : '[name].js'
+const devtool =
+  process.env.NODE_ENV === 'development'
+    ? 'cheap-eval-source-map'
+    : 'source-map'
+const clientFilename =
+  process.env.NODE_ENV === 'production'
+    ? 'assets/[name].[hash].js'
+    : 'assets/[name].js'
 
-const customEnvVars = [
-  'ASSET_PATH'
-]
+const customEnvVars = ['ASSET_PATH']
 
 module.exports = [
   {
     name: 'client',
     target: 'web',
-    entry: process.env.NODE_ENV === 'production'
-      ? '@ninetails-monorepo-react-ssr/app/client.js'
-      : [
-        'webpack-hot-middleware/client?name=client&reload=true',
-        '@ninetails-monorepo-react-ssr/app/client.js'
-      ],
+    entry:
+      process.env.NODE_ENV === 'production'
+        ? '@ninetails-monorepo-react-ssr/app/client.js'
+        : [
+          'webpack-hot-middleware/client?name=client&reload=true',
+          '@ninetails-monorepo-react-ssr/app/client.js'
+        ],
     output: {
       path: `${dist}/client`,
       publicPath: process.env.ASSET_PATH || '/',
@@ -36,7 +41,9 @@ module.exports = [
       process: false
     },
     plugins: [
-      process.env.NODE_ENV === 'production' ? () => undefined : new webpack.HotModuleReplacementPlugin(),
+      process.env.NODE_ENV === 'production'
+        ? () => undefined
+        : new webpack.HotModuleReplacementPlugin(),
       new StatsWriterPlugin({
         filename: 'stats.json'
       }),
@@ -46,12 +53,13 @@ module.exports = [
   {
     name: 'server',
     target: 'node',
-    entry: process.env.NODE_ENV === 'production'
-      ? '@ninetails-monorepo-react-ssr/app/express.js'
-      : [
-        'webpack-hot-middleware/client?name=server',
-        '@ninetails-monorepo-react-ssr/app/express.js'
-      ],
+    entry:
+      process.env.NODE_ENV === 'production'
+        ? '@ninetails-monorepo-react-ssr/app/express.js'
+        : [
+          'webpack-hot-middleware/client?name=server',
+          '@ninetails-monorepo-react-ssr/app/express.js'
+        ],
     output: {
       path: dist,
       filename: 'server.js',
@@ -63,7 +71,9 @@ module.exports = [
       process: false
     },
     plugins: [
-      process.env.NODE_ENV === 'production' ? () => undefined : new webpack.HotModuleReplacementPlugin(),
+      process.env.NODE_ENV === 'production'
+        ? () => undefined
+        : new webpack.HotModuleReplacementPlugin(),
       definePluginFactory(customEnvVars)
     ]
   }
