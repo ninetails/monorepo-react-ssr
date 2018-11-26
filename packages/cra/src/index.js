84e17f33045a4b4a970f4d9b1e278571c7011576
Carlos Kazuo <ninetails.nine@gmail.com>
2018-11-26 14:07:34 -0200

    feat: added CRA

---
+++ packages/cra/src/index.js
@@ -0,0 +1,8 @@
+import * as serviceWorker from './serviceWorker'
+
+import '@ninetails-monorepo-react-ssr/app/client.js'
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
