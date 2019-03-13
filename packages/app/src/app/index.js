9016a06a31f727540c48868aca65c0bdb83521c0
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2019-03-12 22:05:01 -0300

    feat: 🎸 added react-router-dom with SSR

--- packages/app/src/app/index.js
+++ packages/app/src/app/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+
+function Home () {
+  return <h1>Home</h1>
+}
+
+function About () {
+  return <h1>About</h1>
+}
 
 function App () {
   return (
     <div>
-      <h1>working!</h1>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+      </Switch>
     </div>
   )
 }
--- packages/app/src/client.js
+++ packages/app/src/client.js
@@ -1,11 +1,13 @@
 import React from 'react'
-// import { hydrate, render } from 'react-dom'
 import { unstable_createRoot } from 'react-dom' // eslint-disable-line camelcase
 import App from './app'
+import BrowserRouter from './helpers/BrowserRouter'
 import getRoot from './helpers/getRoot'
 
 const app = (
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
 
 function init (root = getRoot(process.env.REACT_APP_ROOT)) {
