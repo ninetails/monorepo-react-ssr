44699ab792b061f46acffa3a74767d830467b92b
Carlos Kazuo <40065+ninetails@users.noreply.github.com>
2019-03-12 21:35:29 -0300

    fix: 🐛 error for importing render function

--- packages/app/src/client.js
+++ packages/app/src/client.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { hydrate, render } from 'react-dom'
-import { unstable_createRoot } from 'react-dom/server' // eslint-disable-line camelcase
+import { unstable_createRoot } from 'react-dom' // eslint-disable-line camelcase
 import App from './app'
 import getRoot from './helpers/getRoot'
 
@@ -9,11 +9,7 @@ const app = (
 )
 
 function init (root = getRoot(process.env.REACT_APP_ROOT)) {
-  // createRoot(root, { hydrate: root.hasChildNodes() }).render(...)
-  unstable_createRoot(root, { hydrate: root.hasChildNodes() }).render(app)
-
-  // const renderFn = root.hasChildNodes() ? hydrate : render
-  // renderFn(app, root)
+  return unstable_createRoot(root, { hydrate: root.hasChildNodes() }).render(app)
 }
 
 init()
